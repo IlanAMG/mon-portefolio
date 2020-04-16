1dacc9a306d036cb4ec524b31807941a8ed14d66
Ilan Amzallag <59542957+IlanAMG@users.noreply.github.com>
2020-04-16 19:19:59 +0200

    v1.1.0

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import './App.css';
 import Home from './components/Home';
 import { Formations } from './components/Formations';
@@ -11,9 +11,12 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import Context from './untils/context';
 import useWindowSize from './untils/useWindowSize'
 import FlecheGoTop from './components/FlecheGoTop';
+import hexagone from './ASSETS/hexagone.png'
 import "animate.css/animate.min.css";
 
 const App = () => {
+  const curseurRef = useRef()
+  const [rotate, setRotate] = useState(0)
   const [isVisible, setIsVisible] = useState(false)
   const [styles, setStyles] = useState({})
   const [styleWallUp, setStyleWallUp] = useState({})
@@ -21,10 +24,41 @@ const App = () => {
   const [transitionFinish, setTransitionFinish] = useState(false)
   const sizeWindow = useWindowSize();
 
+  const mousePos = e => {
+    curseurRef.current.setAttribute('style', `top:${e.pageY - 20}px; left:${e.pageX - 20}px;`)
+  }
+
+  const cursorRotate = () => {
+    setRotate(rotate + 5)
+  }
+
+  const useInterval = (callback, delay) => {
+    const savedCallback = useRef();
+    // Remember the latest callback.
+    useEffect(() => {
+      savedCallback.current = callback;
+    }, [callback]);
+    // Set up the interval.
+    useEffect(() => {
+      function tick() {
+        savedCallback.current();
+      }
+      if (delay !== null) {
+        let id = setInterval(tick, delay);
+        return () => clearInterval(id);
+      }
+    }, [delay]);
+  }
+  
+  useInterval(cursorRotate, 100)
+
   return (
     <Context.Provider value={{ sizeWindow, isVisible, transitionFinish, setTransitionFinish, styleWallUp, setStyleWallUp, styleWallDown, setStyleWallDown }}>
       <Router>
-          <div className='App'>
+          <div onMouseMove={mousePos} className='App'>
+            <div ref={curseurRef} className="curseur-perso">
+              <img src={hexagone} alt="cursor" style={{transform: 'rotate(' + rotate +'deg)'}}/>
+            </div>
             <WallTransition />
             <MenuNav  setIsVisible={setIsVisible} isVisible={isVisible} setStyles={setStyles} styles={styles} />
             <BtnNav isVisible={isVisible} setIsVisible={setIsVisible} />
